e45d23976e545dd9d665f23bca58c68cb7cf2dd5
ARkirE24 <126833041+ARkirE24@users.noreply.github.com>
2023-03-04 15:42:21 -0500

    changes secret

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const fechaInicio = new Date('2022-11-12T00:00:00')
+
+const actualizarContador = setInterval(() => {
+  const tiempoTranscurrido = Date.now() - fechaInicio.getTime();
+
+  const diasTranscurridos = Math.floor(tiempoTranscurrido / (1000 * 60 * 60 * 24));
+  const horasTranscurridas = Math.floor((tiempoTranscurrido % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutosTranscurridos = Math.floor((tiempoTranscurrido % (1000 * 60 * 60)) / (1000 * 60));
+  const segundosTranscurridos = Math.floor((tiempoTranscurrido % (1000 * 60)) / 1000);
+  const mesesTranscurridos = Math.floor(diasTranscurridos / 30);
+
+  const diasRestantes = diasTranscurridos % 30;
+
+  
+  if(mesesTranscurridos < 10) document.getElementById('meses').textContent = '0' + mesesTranscurridos;
+  else document.getElementById('meses').textContent = mesesTranscurridos;
+  if(diasRestantes < 10) document.getElementById('dias').textContent = '0' + diasRestantes;
+  else document.getElementById('dias').textContent = diasRestantes;
+  if(horasTranscurridas < 10) document.getElementById('horas').textContent = '0' + horasTranscurridas;
+  else document.getElementById('horas').textContent = horasTranscurridas;
+  if(minutosTranscurridos < 10) document.getElementById('minutos').textContent = '0' + minutosTranscurridos;
+  else document.getElementById('minutos').textContent = minutosTranscurridos;
+  if(segundosTranscurridos < 10) document.getElementById('segundos').textContent = '0' + segundosTranscurridos;
+  else document.getElementById('segundos').textContent = segundosTranscurridos;
+}, 1000);
 
 let texto = '0'
 let resultado = 113412
@@ -43,3 +68,4 @@ function funcionTeclaNumero(palabra){
   }
   display.innerText = texto
 }
+
